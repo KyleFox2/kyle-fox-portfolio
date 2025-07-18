4abf46b130e0b5581290e0f73e3f964679e2d6ab
Kyle Fox <kylescottfox@gmail.com>
2025-07-18 11:15:09 +0100

    Updated with a blog section

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+"use strict";
+
+// navbar variables
+const nav = document.querySelector(".mobile-nav");
+const navMenuBtn = document.querySelector(".nav-menu-btn");
+const navCloseBtn = document.querySelector(".nav-close-btn");
+
+// navToggle function
+const navToggleFunc = function () {
+  nav.classList.toggle("active");
+};
+
+navMenuBtn.addEventListener("click", navToggleFunc);
+navCloseBtn.addEventListener("click", navToggleFunc);
+
+// menu function
 function toggleMenu() {
   const menu = document.querySelector(".menu-links");
   const icon = document.querySelector(".hamburger-icon");
